3764bed027f90cb61306c577db1a898ead4a34aa
Amodhx <amxdhnanditha@gmail.com>
2024-12-30 23:02:13 +0530

    sign in done with axios

--- src/app/component/login-page/login-page.component.ts
+++ src/app/component/login-page/login-page.component.ts
@@ -6,6 +6,7 @@ import { notes, users } from '../../../db/db';
 import { UserModel } from '../../../model/userModel';
 import { NoteModel } from '../../../model/noteModel';
 import axios from 'axios';
+import swal from 'sweetalert2'
 
 @Component({
   selector: 'app-login-page',
@@ -30,44 +31,31 @@ export class LoginPageComponent {
       this.btnClass = "disabled";
     }
   }
-  onLoginBtnClick(){
-    console.log(this.userNameValue+" e "+ this.emailValue+" p "+this.passwordValue);
+  async onLoginBtnClick(){
+    const payload = { email: this.userNameValue, password: this.passwordValue };
+
+    try {
+      const response = await axios.post('http://localhost:300/api/v1/user/signIn', payload);
+      if (response.status === 201) {
+        localStorage.setItem('jwtKey',response.data)
+        this.router.navigateByUrl('/home');
+      } else {
+        console.log('Unexpected status:', response.status);
+      }
+    } catch (err: any) { // Explicitly typing as `any` to access properties
+      if (err.response && err.response.status === 401 || err.response.status === 403) {
+        swal.fire({
+          title: "The Credentials?",
+          text: "Invalid Credentials!",
+          icon: "error"
+        });
+      } else {
+        console.error('An error occurred:', err);
+        alert('An unexpected error occurred. Please try again later.');
+      }
+    }
+
     
-    // this.router.navigateByUrl('/home');
-
-//     const http =new XMLHttpRequest();
-//     const user:UserModel =new UserModel("",this.userNameValue,"",this.emailValue,this.passwordValue,"");
-//     http.onreadystatechange =() =>{
-//         //check state
-//         if (http.readyState == 4){
-//             if (http.status == 200){
-//                 const data = JSON.parse(http.responseText);
-// -                
-//                   users.push(new UserModel(data.userId,data.firstName,data.lastName,data.email,data.password,data.profilePic));
-                
-//                   if(data.email == this.emailValue && data.password == this.passwordValue){
-//                          this.router.navigateByUrl('/home');
-//                   }else{
-//                     alert("It Seems Wrong Credential !!!! Try Again!");
-//                   }
-               
-
-
-//             }else {
-//                 console.error("Failed");
-//                 console.error("Status Received" , http.status);
-//                 console.error("Processing Stage" , http.readyState);
-//             }
-//         }else{
-//             console.log("Processing stage", http.readyState);
-//         }
-//     }
- 
-//          http.open("GET","http://localhost:8080/api/v1/users/getUser",true);
-//          http.setRequestHeader("_user",JSON.stringify(user));
-//          http.setRequestHeader("Content-Type","application/json");
-//          http.send();
-  
     
   }
 
